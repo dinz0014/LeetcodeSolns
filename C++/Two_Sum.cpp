413fc9a8ba49edf492b206d28b848b1d59bb6ae8
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2022-02-19 13:19:25 +1100

    Added c++ solution to Two Sum problem

---
+++ C++/Two_Sum.cpp
@@ -0,0 +1,25 @@
+#include <unordered_map>
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> valMap;
+        int N = nums.size();
+        vector<int> result;
+        
+        for (int i = 0; i < N; i++)
+        {
+            if (valMap.find(target - nums[i]) != valMap.end())
+            {
+                result = {i, valMap[target - nums[i]]};
+                break;
+            }
+            else
+            {
+                valMap[nums[i]] = i;   
+            }
+        }
+        
+        return result;
+    }
+};
