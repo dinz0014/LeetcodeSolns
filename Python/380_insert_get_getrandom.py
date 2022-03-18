1ec6e944cafb3fa5abf0364c124a3eea49e7344c
Dinujaya Rajakaruna <dinujayarajakaruna@gmail.com>
2022-03-18 19:56:59 +1100

    Added python solution to problem 380

---
+++ Python/380_insert_get_getrandom.py
@@ -0,0 +1,40 @@
+from random import randint
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.iMap = {} # maps value to index in array
+        self.array = [] # stores value in a order (for O(1) get random)
+        self.length = 0 # variable to track size of set (for O(1) size checking)
+        
+    def insert(self, val: int) -> bool:
+        # Immediately return false if the value is in the map
+        if val in self.iMap:
+            return False
+        
+        # Add new elements to end of list
+        self.iMap[val] = self.length
+        self.array.append(val)
+        self.length += 1
+        return True
+
+    def remove(self, val: int) -> bool:
+        # Immediately return false if the value is not in the map
+        if val not in self.iMap:
+            return False
+        
+        # Copy item at end of list to index of item being removed
+        rmIdx = self.iMap[val]
+        self.array[rmIdx] = self.array[-1]
+        self.iMap[self.array[-1]] = rmIdx
+
+        # Delete item from array and map, decrement length too
+        del self.iMap[val]
+        self.array.pop()
+        self.length -= 1
+        return True
+
+    def getRandom(self) -> int:
+        # Get random index and return element at this index
+        randomIdx = randint(0, self.length - 1)
+        return self.array[randomIdx]
